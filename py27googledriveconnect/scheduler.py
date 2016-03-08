3d48d3eea98fd66ed960324ea8449a45bd65d217
kathryncogert <cogerk@gmail.com>
2016-03-08 10:35:37 -0800

    Debugging google drive utils & scheduling

--- py27googledriveconnect/scheduler.py
+++ py27googledriveconnect/scheduler.py
@@ -5,9 +5,9 @@ s = sched.scheduler(time.time, time.sleep)
 
 
 def main(sc):
-    print 'Collecting Data...'
+    print 'Querying Reactor'
     write_to_reactordrive(1, 'R1data')
-    sc.enter(10, 1, main, (sc,))
+    sc.enter(30, 1, main, (sc,))
 
-s.enter(10, 1, main, (s,))
+s.enter(30, 1, main, (s,))
 s.run()
