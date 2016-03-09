0039cb991bffd4ff692e77984d2215d4d4c76f52
manewton <manewton@uw.edu>
2016-03-09 14:28:37 -0800

    Added streaming example. Also my changes to bokehplot.py

--- Project/bokehplot.py
+++ Project/bokehplot.py
@@ -1,9 +1,11 @@
 import pandas as pd
-
+import time
 from bokeh.io import curdoc, show, vform, output_file
 from bokeh.models import ColumnDataSource, VBox, HBox, Select, CheckboxButtonGroup
-from bokeh.plotting import Figure
+from bokeh.plotting import Figure, output_server, curdoc, show
 
+# Prepare output to server
+output_server("BioReactor_Data_Logging")
 
 # Accept/Setup Dataframe to be Plotted
 sample_data_live = 'sampledatalive.csv'
@@ -57,3 +59,10 @@ controls = VBox(data_select)
 curdoc().add_root(HBox(controls, plot))
 output_file("bokehplot.html", title="Bokeh Line Plot")
 show(HBox(controls, plot))
+
+while True:
+    # Somehow need to update the data of the source object "source"
+
+    # Store the updated source on the server
+    curdoc().store_objects(source)
+    time.sleep(0.5)
