afccc12a8ab1efd7eb410da1a4d7aba37ee34f40
kathryncogert <cogerk@gmail.com>
2016-03-11 14:29:50 -0800

    Kathryn revises some google drive stuff

--- py27googledriveconnect/downloader.py
+++ py27googledriveconnect/downloader.py
@@ -3,11 +3,13 @@ import os
 from googledriveutils import read_from_reactordrive
 from googledriveutils import remove_file
 
-def download_latest(reactorno=1, filename='R1data.csv',):
+def download_latest(reactorno=1, filename='R1data',):
     curdir = os.getcwd()
     pardir = os.path.abspath(os.path.join(curdir, os.pardir))
-    save_to = pardir+'/SampleData/'+filename
+    save_to = pardir+'/DataManagement/'+filename
     if os.path.isfile(save_to):
         remove_file(save_to)
     read_from_reactordrive(reactorno, filename, save_to)
     return
+
+download_latest()
