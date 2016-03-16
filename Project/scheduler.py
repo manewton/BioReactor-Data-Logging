fe561bc0bc7e1d88f911404c6075d7a423bc3a29
kathryncogert <cogerk@gmail.com>
2016-03-15 17:59:49 -0700

    Added even more comments and documentation. YAY!!!!

--- Project/scheduler.py
+++ Project/scheduler.py
@@ -1,13 +1,25 @@
+"""
+Written By: Kathryn Cogert
+For: Winkler Lab/CSE599 Winter Quarter 2016
+Purpose: Read reactor data to google drive every 30 secs.
+"""
+
 import sched
 import time
 from googledriveutils import write_to_reactordrive
+
 s = sched.scheduler(time.time, time.sleep)
 
 
 def main(sc):
+    """
+    Queries reactor for latest data and appends to google drive file every 30s.
+    :param sc: run again.
+    :return:
+    """
     print 'Querying Reactor'
     write_to_reactordrive(1, 'R1data')
-    sc.enter(1, 1, main, (sc,))
+    sc.enter(30, 1, main, (sc,))
 
-s.enter(1, 1, main, (s,))
+s.enter(30, 1, main, (s,))
 s.run()
